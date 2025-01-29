ed409d0e58d5098acee7520619812ad31d11579a
divesh2022 <165630306+divesh2022@users.noreply.github.com>
2025-01-29 14:19:26 +0530

    Create main_cpp.cpp

---
+++ main_cpp.cpp
@@ -0,0 +1,33 @@
+// main function
+template <typename T>
+int main()
+{
+    cout<<" valid data types are int , float , double , char , string \n";
+    string datatype = NULL;
+    cout<<"enter data type of the array\n";
+    cin>>datatype;
+    if (datatype == "int")
+    {
+        //code
+    }
+    else if (datatype == "char")
+    {
+        //code
+    }
+    else if (datatype == "float")
+    {
+        //code
+    }
+    else if (datatype == "double")
+    {
+        //code
+    }
+    else if (datatype == "string")
+    {
+        //code
+    }
+    else
+    {
+        cout<<"invalid data type\n";
+    }
+}
