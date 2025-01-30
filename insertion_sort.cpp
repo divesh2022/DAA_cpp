68dd73cf0f5e06d74225699be16752d03a1e211b
divesh2022 <165630306+divesh2022@users.noreply.github.com>
2025-01-30 16:03:08 +0530

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,5 +1,3 @@
-// insertion sort 
-
 #include <iostream>
 #include <string>
 using namespace std;
@@ -7,38 +5,39 @@ using namespace std;
 template <typename T>
 void insertion_sort(T A[], int size)
 {
-    for (int i = 1; i < size; ++i)
+    for (int i = 1 ; i < size ; ++i)
     {
         T key = A[i];
         int j = i - 1;
-        while (j >= 0 && key < A[j])
+        while(j >= 0 && key < A[j])
         {
-            A[j + 1] = A[j];
-            j = j - 1;
+            A[j+1] = A[j];
+            j = j-1;
         }
-        A[j + 1] = key;
+        A[j+1] = key;
     }
 }
 
 template <typename T>
 void show(T A[], int size)
 {
-    for (int i = 0; i < size; ++i)
+    for (int i = 0 ; i < size; ++i)
     {
-        cout << "A[" << i << "] = " << A[i] << endl;
+        cout << " A[ " << i << " ] = " << A[i] << endl;
     }
 }
 
-
-int main()
+int main ()
 {
-    cout << "Valid data types are int, float, double, char, string\n";
-    string datatype;
-    cout << "Enter data type of the array: ";
-    cin >> datatype;
-    
-    if (datatype == "int")
+    char ch = 'y';
+    while(ch =='y')
     {
+        cout << "valid data types are int, float, double, char, string\n";
+        string datatype;
+        cout << "Enter data type of the array: ";
+        cin >> datatype;
+        if ( datatype == "int")
+        {
         int size;
         cout << "Enter the size of the array: ";
         cin >> size;
@@ -54,83 +53,103 @@ int main()
         cout << "After sorting:\n";
         show(A, size);
         delete[] A;
-    }
-    else if (datatype == "char")
-    {
-        int size;
-        cout << "Enter the size of the array: ";
-        cin >> size;
-        char* A = new char[size];
-        for(int i = 0; i < size; i++)
-        {
-            cout << "Enter the element at index " << i << ": ";
-            cin >> A[i];
         }
-        cout << "Before sorting:\n";
-        show(A, size);
-        insertion_sort(A, size);
-        cout << "After sorting:\n";
-        show(A, size);
-        delete[] A;
-    }
-    else if (datatype == "float")
-    {
-        int size;
-        cout << "Enter the size of the array: ";
-        cin >> size;
-        float* A = new float[size];
-        for(int i = 0; i < size; i++)
+        else if (datatype == "float")
         {
-            cout << "Enter the element at index " << i << ": ";
-            cin >> A[i];
+            int size;
+            cout << "Enter the size of the array: ";
+            cin >> size;
+            float* A = new float[size];
+            for(int i = 0; i < size; i++)
+            {
+                cout << "Enter the element at index " << i << ": ";
+                cin >> A[i];
+            }
+            cout << "Before sorting:\n";
+            show(A, size);
+            insertion_sort(A, size);
+            cout << "After sorting:\n";
+            show(A, size);
+            delete[] A;
         }
-        cout << "Before sorting:\n";
-        show(A, size);
-        insertion_sort(A, size);
-        cout << "After sorting:\n";
-        show(A, size);
-        delete[] A;
-    }
-    else if (datatype == "double")
-    {
-        int size;
-        cout << "Enter the size of the array: ";
-        cin >> size;
-        double* A = new double[size];
-        for(int i = 0; i < size; i++)
+        else if (datatype == "double")
         {
-            cout << "Enter the element at index " << i << ": ";
-            cin >> A[i];
+            int size;
+            cout << "Enter the size of the array: ";
+            cin >> size;
+            double* A = new double[size];
+            for(int i = 0; i < size; i++)
+            {
+                cout << "Enter the element at index " << i << ": ";
+                cin >> A[i];
+            }
+            cout << "Before sorting:\n";
+            show(A, size);
+            insertion_sort(A, size);
+            cout << "After sorting:\n";
+            show(A, size);
+            delete[] A;
         }
-        cout << "Before sorting:\n";
-        show(A, size);
-        insertion_sort(A, size);
-        cout << "After sorting:\n";
-        show(A, size);
-        delete[] A;
-    }
-    else if (datatype == "string")
-    {
-        int size;
-        cout << "Enter the size of the array: ";
-        cin >> size;
-        string* A = new string[size];
-        for(int i = 0; i < size; i++)
+        else if (datatype == "char")
         {
-            cout << "Enter the element at index " << i << ": ";
-            cin >> A[i];
+            int size;
+            cout << "Enter the size of the array: ";
+            cin >> size;
+            char* A = new char[size];
+            for(int i = 0; i < size; i++)
+            {
+                cout << "Enter the element at index " << i << ": ";
+                cin >> A[i];
+            }
+            cout << "Before sorting:\n";
+            show(A, size);
+            insertion_sort(A, size);
+            cout << "After sorting:\n";
+            show(A, size);
+            delete[] A;
         }
-        cout << "Before sorting:\n";
-        show(A, size);
-        insertion_sort(A, size);
-        cout << "After sorting:\n";
-        show(A, size);
-        delete[] A;
+        else if (datatype == "string")
+        {
+            int size;
+            cout << "Enter the size of the array: ";
+            cin >> size;
+            string* A = new string[size];
+            for(int i = 0; i < size; i++)
+            {
+                cout << "Enter the element at index " << i << ": ";
+                cin >> A[i];
+            }
+            cout << "Before sorting:\n";
+            show(A, size);
+            insertion_sort(A, size);
+            cout << "After sorting:\n";
+            show(A, size);
+            delete[] A;
+        }
+        else
+        {
+            cout << "Invalid data type\n";
+        }
+        
+        cout<<"Do you want to continue? (y/n): ";
+        cin>>ch;
+            if(ch == 'n')
+    {
+        cout << "Goodbye!\n";
+    }
+    else if(ch == 'y')
+    {
+        continue;
+    }
+    else if(ch != 'y' || ch != 'n')
+    {
+        cout << "Invalid input\n";
     }
     else
     {
-        cout << "Invalid data type\n";
+        exit(0);
     }
 
+    }
     return 0;
 }
