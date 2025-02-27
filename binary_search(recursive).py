acf3d61e0fc60aa77e9468696661de4223b7c8c3
divesh2022 <165630306+divesh2022@users.noreply.github.com>
2025-02-27 19:50:20 +0530

    Update binary_search(recursive).py

--- binary_search(recursive).py
+++ binary_search(recursive).py
@@ -1,4 +1,4 @@
-def binary_search(lst, start, stop, key):
+def binary_search_i(lst, start, stop, key):
     if start > stop:
         return False
     else:
@@ -9,19 +9,39 @@ def binary_search(lst, start, stop, key):
             return binary_search(lst, start, mid-1, key)
         else:
             return binary_search(lst, mid+1, stop, key)
-def main():
-    lst = input("Enter the list of numbers separated by hyphens: ")
-    lst = lst.split("-")
-    print("Unsorted list is", lst)
-    num_list = [int(i) for i in lst]
-    sorted_list = sorted(num_list)
-    print("Sorted list is", sorted_list)
-    key = int(input("Enter value to find: "))
-    res = binary_search(sorted_list, 0, len(sorted_list) - 1, key)
-    if res != False:
+def binary_search_r(lst, key):
+    start, stop = 0, len(lst) - 1
+    while start <= stop:
+        mid = (start + stop) // 2
+        if key == lst[mid]:
+            return mid
+        elif key < lst[mid]:
+            stop = mid - 1
+        else:
+            start = mid + 1
+    return False
+def check(res):
+        if res != False:
         print("Element found at index", res)
     else:
         print("Element not found")
+def main():
+    lst = input("Enter the list of numbers separated by hyphens: ")
+    lst = lst.split("-")
+    num_list = [int(i) for i in lst]
+    print("Unsorted list is", lst)
+    sorted_list = sorted(num_list)
+    print("Sorted list is", sorted_list)
+    key = int(input("Enter value to find: "))
+    a = input("enter your choice  : ")
+    if (a == 0):
+        res1 = binary_search(sorted_list, 0, len(sorted_list) - 1, key)
+        check(res1)
+    elif (a == 1):
+        res2 = binary_search_i(sorted_list, 0, len(sorted_list) - 1, key)
+        check(res2)
+    else:
+        print("invalid choice")
 n = int(input("Enter the number of test cases: "))
 for i in range(n):
     main()
