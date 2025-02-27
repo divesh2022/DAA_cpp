ed377ac4102978194b616b3743f6c6849adf370b
divesh2022 <165630306+divesh2022@users.noreply.github.com>
2025-02-27 19:34:04 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -9,16 +9,6 @@ def binary_search(lst, start, stop, key):
             return binary_search(lst, start, mid-1, key)
         else:
             return binary_search(lst, mid+1, stop, key)
-
-def sorting(lst):
-    for _ in range(len(lst)):
-        for j in range(len(lst)-1):
-            if lst[j] > lst[j+1]:
-                temp = lst[j]
-                lst[j] = lst[j+1]
-                lst[j+1] = temp
-    return lst
-
 def main():
     lst = input("Enter the list of numbers separated by hyphens: ")
     lst = lst.split("-")
@@ -26,16 +16,14 @@ def main():
     a = []
     for i in lst:
         a.append(int(i))
-    a = sorting(a)
+    a = sorted(a)
     print("Sorted list is", a)
     key = int(input("Enter value to find: "))
-    print("Sorted list is", a)
     res = binary_search(a, 0, len(a)-1, key)
     if res != False:
         print("Element found at index", res)
     else:
         print("Element not found")
-
 n = int(input("Enter the number of test cases: "))
 for i in range(n):
     main()
